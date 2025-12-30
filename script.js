12b832e9f82414095effb6f5df0df416dfe46f22
h3cat3 <dumitrescu.c.e@gmail.com>
2025-12-30 14:38:07 +0000

    styling
    change the style to Tailwing Css

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
 document.getElementById("form").addEventListener('submit', (ev) => {
     ev.preventDefault();
     const email = document.getElementById("email");
+    const errorMsg =document.getElementById("error");
     const regex = /^[\w\-\.]+@([\w-]+\.)+[\w-]{2,}$/;
     if (email.value === "" || !email.value.match(regex)) {
-        email.classList.add("border-danger")
+        email.classList.add("border-4", "border-red-500")
+        errorMsg.style.display = "block";
+
     }
     else {
-        email.classList.remove("border-danger");
+        email.classList.remove("border-4", "border-red-500");
+        errorMsg.style.display = "none";
+       
+
     }
 })
